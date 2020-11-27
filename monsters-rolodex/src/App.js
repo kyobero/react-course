b9c23c6e50d5a26ec12093f15e097316d532a788
Ronald <kyoberonald@gmail.com>
2020-11-27 17:45:23 +0300

    created the search box component+added style to it and moved the search functionality to the search-box component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -26,9 +27,10 @@ componentDidMount(){
 
     return (
       <div className= 'App'>
-        <input type='search' 
+    
+        <SearchBox
           placeholder='search monsters' 
-          onChange={e => this.setState({ searchField: e.target.value })} 
+          handleChange=e => this.setState({ searchField: e.target.value })
         />
         <CardList monsters={filteredMonsters} />
           
---
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+    <input 
+        className='search'
+        type='search' 
+        placeholder={placeholder} 
+        onChange={handleChange} 
+  />
+
+)
